0f5d6453b295002edea0543d36806380930377c5
Mohamad masrur as ari <50537339+masrurAS@users.noreply.github.com>
2020-06-25 20:35:08 +0700

    init

--- assets/config.js
+++
@@ -1,7 +0,0 @@
-var CONFIG = {
-  link: {
-    fb: "//facebook.com/nduoseh",
-    ig: "//instagram.com/nduoseh_",
-    yt: "//youtube.com/channel/UCNbMRoTIoiHLF6Kw7UDNOiw"
-  }
-}
--- assets/script.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-  // pasang link
-  $('.link').each(function () {
-    var key = $(this).data('key');
-    $(this).attr('href', CONFIG.link[key]);
-  });
-  
-  
-});
